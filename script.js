0e426871c83ec5e022fe7e802160eb62d028cf46
Rhuan Henrique <87444404+rhuan05@users.noreply.github.com>
2021-12-23 00:36:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// Efeito Slider
+
+var slider = document.querySelector('.container--slider');
+var sliderProjetos = document.querySelector('.container--slider--projetos');
+var scrollTech = 0;
+var scrollProjetos = 0;
+var widthTela = window.screen.width;
+
+if(widthTela <= 410){
+    function setaEsquerda(){
+        if(scrollProjetos == 0){
+            scrollProjetos = 0;
+        } else{
+            scrollProjetos = scrollProjetos + 205;
+            slider.style.marginLeft = scrollProjetos + 'px';
+        };
+    }
+
+    function setaDireita(){
+        if(scrollProjetos <= -410){
+            scrollProjetos = 0;
+            slider.style.marginLeft = scrollProjetos + 'px';
+        } else{
+            scrollProjetos = scrollProjetos - 205;
+            slider.style.marginLeft = scrollProjetos + 'px';
+        };
+    }
+} else {
+    function setaEsquerda(){
+        if(scrollTech == 0){
+            scrollTech = 0;
+        } else{
+            scrollTech = scrollTech + 300;
+            slider.style.marginLeft = scrollTech + 'px';
+        };
+    }
+
+    function setaDireita(){
+        if(scrollTech == -600){
+            scrollTech = 0;
+            slider.style.marginLeft = scrollTech + 'px';
+        } else{
+            scrollTech = scrollTech - 300;
+            slider.style.marginLeft = scrollTech + 'px';
+        };
+    }
+};
+
+var menuCelular = document.querySelector('.menuCelular');
+
+menuCelular.addEventListener('click', ()=>{
+    document.querySelector('header').classList.toggle('menuClicado');
+});
+
+
+// Scroll Animado do Menu
+
+const menuItems = document.querySelectorAll('a[href^="#"]');
+
+menuItems.forEach(item =>{
+    item.addEventListener('click', scrollToIdOnClick);
+});
+
+function scrollToIdOnClick(event){
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+
+    window.scroll({
+        top: to - 20,
+        behavior: 'smooth'
+    });
+};
